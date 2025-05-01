85acdc2bf47b4fda1c71049aa1ed0cc176d0b4a8
Lewis O-Schneider <loschnei@usc.edu>
2025-05-01 06:03:52 +0000

    Parts 1 and 2 done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,12 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool backtrack(const AvailabilityMatrix& avail, 
+                const size_t dailyNeed,
+                const size_t maxShifts, 
+                DailySchedule& sched, 
+                std::vector<int>& shiftsAssigned,
+                size_t day, size_t slot);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +43,48 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    const size_t n = avail.size();
+    const size_t k = avail[0].size();
 
+    sched.resize(n);
 
+    std::vector<int> shiftsAssigned(k, 0);
 
+    return backtrack(avail, dailyNeed, maxShifts, sched, shiftsAssigned, 0, 0);
 }
 
+bool backtrack(const AvailabilityMatrix& avail, 
+                const size_t dailyNeed,
+                const size_t maxShifts, 
+                DailySchedule& sched, 
+                std::vector<int>& shiftsAssigned,
+                size_t day, size_t slot) {
+    
+    size_t n = avail.size();
+    size_t k  = avail[0].size();
+
+    if (day == n) {
+        return true;
+    }
+
+    if (slot == dailyNeed) {
+        return backtrack(avail, dailyNeed, maxShifts, sched, shiftsAssigned, day+1, 0);
+    }
+
+    for (size_t worker = 0; worker < k; ++worker) {
+        if (avail[day][worker] && shiftsAssigned[worker] < static_cast<int>(maxShifts) && std::find(sched[day].begin(), sched[day].end(), worker) == sched[day].end()) {
+            sched[day].push_back(worker);
+            shiftsAssigned[worker]++;
+
+            if (backtrack(avail, dailyNeed, maxShifts, sched, shiftsAssigned, day, slot+1)) {
+                return true;
+            }
+
+            sched[day].pop_back();
+            shiftsAssigned[worker]--;
+        }
+
+    }
+    return false;
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordlehelper(const std::string& in,
+                  const std::string& floating,
+                  const std::set<std::string>& dict,
+                  const std::vector<std::string>& possible,
+                  std::string& current,
+                  int index,
+                  std::set<std::string>& result);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,6 +30,73 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    int size = in.size();
+    std::vector<std::string> possible(size);
+
+    for (std::set<std::string>::const_iterator it = dict.begin(); it != dict.end(); ++it) {
+        const std::string& temp = *it;
+        if (temp.size() != size) {
+        } else {
+            bool fixed = true;
+            for (int i = 0; i < size; ++i) {
+                if (temp[i] != in[i] && in[i] != '-') {
+                    fixed = false;
+                    break;
+                }
+            }
+            if (fixed) {
+                for (int i = 0; i < size; ++i) {
+                    if (in[i] == '-') {
+                        char c = temp[i];
+                        if (std::islower(c)) {
+                            if(possible[i].find(c) == std::string::npos) {
+                                possible[i].push_back(c);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    std::set<std::string> result;
+    std::string current = in;
+    wordlehelper(in, floating, dict, possible, current, 0, result);
+    return result;
 }
 
 // Define any helper functions here
+void wordlehelper( const std::string& in,
+                   const std::string& floating,
+                   const std::set<std::string>& dict,
+                   const std::vector<std::string>& possible,
+                   std::string& current, 
+                   int index, 
+                   std::set<std::string>& result) {
+    int size = in.size();
+    if (index == size) {
+        std::string temp = current;
+        for (int i = 0; i < floating.size(); ++i) {
+            char tempc = floating[i];
+            if (temp.find(tempc) == std::string::npos) {
+                return;
+            }
+            temp[temp.find(tempc)] = '-';
+        }
+        if (dict.find(current) != dict.end()) {
+            result.insert(current);
+        }
+        return;
+    }
+
+    if (in[index] != '-') {
+        wordlehelper(in, floating, dict, possible, current, index + 1, result);
+    } else {
+        const std::string choice = possible[index];
+        for(int i = 0; i < choice.size(); ++i) {
+            current[index] = choice[i];
+            wordlehelper(in, floating, dict, possible, current, index + 1, result);
+        }
+        current[index] = '-';
+    }
+}
